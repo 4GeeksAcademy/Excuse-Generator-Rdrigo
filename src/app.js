46dcec55df9d3b93f644362bc048ad562116afcb
Rodrigou0 <127239242+Rodrigou0@users.noreply.github.com>
2023-05-08 19:34:21 +0000

    excuse

--- src/app.js
+++ src/app.js
@@ -6,6 +6,26 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#btn").addEventListener("click", () => {
+    document.querySelector("#the-excuse").innerHTML = generateExcuse();
+  });
+};
+let generateExcuse = () => {
+  let pronoun = ["A", "The"];
+  let subject = ["grandma", "rooster", "dog", "driver", "comedian", "Doctor"];
+  let action = ["took my", "threw my", "yelled at my", "broke my", " took my"];
+  let possession = ["book", "toe", "tang", "shoe"];
+  let where = ["on the street", "on the trash", "on the roof", "in the car"];
+
+  return (
+    pronoun[Math.floor(Math.random() * pronoun.length)] +
+    " " +
+    subject[Math.floor(Math.random() * subject.length)] +
+    " " +
+    action[Math.floor(Math.random() * action.length)] +
+    " " +
+    possession[Math.floor(Math.random() * possession.length)] +
+    " " +
+    where[Math.floor(Math.random() * where.length)]
+  );
 };
